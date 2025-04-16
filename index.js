91d2be34dda0ebc5b302c373895ebdf236db0e3c
munyac <muchenje.munyaradzi@gmail.com>
2025-04-16 11:14:43 +0200

    inital commit

---
+++ index.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+// const bodyParser = require('body-parser');
+// const prisma = require('./lib/db');
+// const { mail } = require('./lib/nodemailer');
+// const { generateTwoFactorToken } = require('./lib/token');
+// const { getTwoFactorTokenByEmail } = require('./data');
+// const db = require('./db');
+
+const app = express();
+
+app.use(cors(corsOptions));
+app.use(express.static('public'));
+
+app.use(express.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+
+})
+
+const PORT = process.env.PORT || 8444;
+
+app.listen(PORT, () => {
+    console.log('server running at http://localhost:8444');
+});
