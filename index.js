6f5491ca5a6ab7c59531ed0774301e68b8caca60
munyac <muchenje.munyaradzi@gmail.com>
2025-04-22 10:45:55 +0200

    added new look

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
 // const prisma = require('./lib/db');
 // const { mail } = require('./lib/nodemailer');
 // const { generateTwoFactorToken } = require('./lib/token');
@@ -9,23 +9,27 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 app.use(express.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
-app.post('/', (req, res) => {
-    console.log(req.body)
-    return res.render('./')
-})
+app.post("/", (req, res) => {
+  console.log(req.body);
+  return res.render("./");
+});
+
+app.get("*", (req, res) => {
+  res.redirect("https://google.com");
+});
 
 const PORT = process.env.PORT || 8444;
 
 app.listen(PORT, () => {
-    console.log('server running at http://localhost:8444');
+  console.log("server running at http://localhost:8444");
 });
