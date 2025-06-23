18738b1929be2f953e3a90ba5aaf5b1687f8c68c
Jonathan K <jonathandavikazzy@gmail.com>
2025-06-23 13:17:46 +0200

    Have form data submitting to local database

--- index.js
+++ index.js
@@ -1,35 +1,31 @@
-require("dotenv").config();
-const express = require("express");
-const bodyParser = require("body-parser");
-
+import express from 'express';
+import bodyParser from 'body-parser';
+import pool, { database, insertUserData } from './database.js' ;
 const app = express();
 
 app.use(express.static("public"));
-
 app.use(express.json());
-
-app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
-
-app.use(express.static(__dirname + "/public"));
+app.use(express.static("/public"));
 
 app.get("/", (req, res) => {
-  // let hostname = req.body['server-address']
-  // console.log(req.body)
-  return res.render("./", {
-    // hostname,
-    // error: req.body.error
-  });
-});
+  res.render("index.ejs");
 
-app.get("/{*splat}", (req, res) => {
-  console.log(req.body)
-  res.redirect("https://google.com");
-});
+})
 
-const PORT = process.env.PORT || 8445;
+app.post("/success", (req, res) => {
+  const data = req.body;
+  insertUserData(data);
+  res.render('success.ejs');
+}) 
+
+// app.get("/{*splat}", (req, res) => {
+//    res.redirect("https://google.com");
+//  });
+
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
-  console.log("server running at http://localhost:8445");
+  console.log(`server running at http://localhost:${PORT}`);
 });
