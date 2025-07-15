d65e800dc55dad3d1aa7c70b6f4d1aa4c9e224b8
Jonathan K <jonathandavikazzy@gmail.com>
2025-07-15 08:48:25 +0200

    changed bad get routes to post and changed index route to /login

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
-app.post("/", (req, res) => {
+app.post("/login", (req, res) => {
   res.render("index.ejs");
 })
 
